218a5678e73ea18d3fecbd1691b56443cc493245
Enes Karpuz <eneskarpuz@gmail.com>
2023-10-10 20:33:13 +0300

    KurTablosu DTO'su oluşturuldu.

---
+++ NKredi.DataAccessLayer/Entities/KurTablosu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NKredi.DataAccessLayer.Entities
+{
+    public class KurTablosu
+    {
+        public int Id { get; set; }
+        public string DovizKodu { get; set; }
+        public int Birim { get; set; }
+        public string DovizCinsi { get; set; }
+        public int DovizAlis { get; set; }
+        public int DovizSatis { get; set; }
+        public int EfektifAlis { get; set; }
+        public int EfektifSatis { get; set; }
+    }
+}
